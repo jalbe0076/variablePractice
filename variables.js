2eaede02c1b68b7051ff7ca1ee269d2a42567d99
Jason Alberto <alberto.jason@gmail.com>
2023-02-03 22:32:59 -0500

    Changed variables added outputs

--- variables.js
+++ variables.js
@@ -6,3 +6,11 @@ var year = 2023
 var travelTimeWork = 30
 var paidDeposit = true
 var paidTuition = false
+
+name = "Miru"
+email = "newemail@gmail.com"
+location = "Ottawa"
+
+console.log(name)
+console.log(email)
+console.log(location)
